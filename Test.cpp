210d4eee847cfcdf24a3d8e9e7d5e4076069f898
Earving Morales <earving_08@hotmail.hotmail.com>
2019-04-24 18:50:39 -0500

    created Test Class

---
+++ Test.cpp
@@ -0,0 +1,16 @@
+/**
+*	@file Test.cpp
+*	@author Earving Morales
+*	@date 4/24/19
+*	@brief tests linked list
+*/
+
+#include "LinkedListOfInts"
+#include "Test.h"
+#include <iostream>
+
+using namespace std;
+
+Test:Test(){
+
+}
---
+++ Test.h
@@ -0,0 +1,21 @@
+/**
+*	@file Test.hpp
+*	@author Earving
+*	@date 4/24/19
+*	@brief A header for Test Class
+*/
+
+#ifndef TEST_H
+#define TEST_H
+
+#include "LinkedListOfInts"
+
+class Test {
+private:
+    
+public:
+    Test();
+
+};
+
+#endif
