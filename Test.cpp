07f3bd91ae94fa7e7dc60d8b4451bb1a76101b89
Earving Morales <earving_08@hotmail.hotmail.com>
2019-04-25 23:45:58 -0500

    declared all test functions

--- Test.cpp
+++ Test.cpp
@@ -24,76 +24,186 @@ void Test::runTests(){
     printResult(test06());
     printResult(test07());
     printResult(test08());
+    printResult(test09());
+    printResult(test10());
+    printResult(test11());
+    printResult(test12());
+    printResult(test13());
+    printResult(test14());
+    printResult(test15());
+    printResult(test16());
+    printResult(test17());
+    printResult(test18());
+    printResult(test19());
+    printResult(test20());
+    printResult(test21());
+    printResult(test22());
+    printResult(test23());
+    printResult(test24());
+    printResult(test25());
+    printResult(test26());
+    printResult(test27());
+    printResult(test28());
 }
 
 
 bool Test::test01(){
     LinkedListOfInts list;
-    cout << "Test 1: isEmpty() returns true with an empty list";
+    cout << "Test 1: isEmpty() on empty list returns true";
     return(list.isEmpty());
 }
 
 bool Test::test02(){
     LinkedListOfInts list;
     list.addFront(1);
-    cout << "Test 2: isEmpty() returns false with a nonempty list";
+    cout << "Test 2: isEmpty() returns false after addFront()";
     return(!list.isEmpty());
 }
 
 bool Test::test03(){
-    cout << "Test 3: size() returns 0 when empty list";
+    cout << "Test 3: ";
+    return true;
+}
+
+bool Test::test04(){
+    cout << "Test 4: size() returns 0 when empty list";
     LinkedListOfInts list;
     return(list.size() == 0);
 }
 
-bool Test::test04(){
-    cout << "Test 4: size() returns 1 when list of 1 int";
+bool Test::test05(){
+    cout << "Test 5: size() returns correctly after 1 addFront()";
     LinkedListOfInts list;
     list.addFront(2);
     return(list.size() == 1);
 }
 
-bool Test::test05(){
-    cout << "Test 5: use addFront() once to empty list and check that it is at front of list";
+bool Test::test06(){
+    cout << "Test 6: ";
+    return true;
+}
+
+bool Test::test07(){
+    cout << "Test 7: ";
+    return true;
+}
+
+bool Test::test08(){
+    cout << "Test 8: ";
+    return true;
+}
+
+bool Test::test09(){
+    cout << "Test 9: addFront() properly adds to empty list";
     LinkedListOfInts list;
     list.addFront(3);
     return(list.toVector().front() == 3);
 }
 
-bool Test::test06(){
-    cout << "Test 6: use addFront() twice to empty list and check that the second is at front of list";
+
+bool Test::test10(){
+    cout << "Test 10: addBack() properly adds to empty list";
+    LinkedListOfInts list;
+    list.addBack(5);
+    return(list.toVector().back() == 5);
+}
+
+bool Test::test11(){
+    cout << "Test 11: nth addFront() properly adds to front of list";
     LinkedListOfInts list;
     list.addFront(3);
     list.addFront(4);
     return(list.toVector().front() == 4);
 }
 
-bool Test::test07(){
-    cout << "Test 7: use addBack() once to empty list and check that it is at back of array";
-    LinkedListOfInts list;
-    list.addBack(5);
-    return(list.toVector().back() == 5);
-}
-
-bool Test::test08(){
-    cout << "Test 8: use addBack() twice to empty list and check that the second is at back of array";
+bool Test::test12(){
+    cout << "Test 12: nth addBack() properly adds to the back of the list";
     LinkedListOfInts list;
     list.addBack(5);
     list.addBack(6);
     return(list.toVector().back() == 6);
 }
-bool Test::test09(){
+
+bool Test::test13(){
+    cout << "Test 13: ";
     return true;
 }
-bool Test::test10(){
+
+bool Test::test14(){
+    cout << "Test 14: ";
     return true;
 }
-bool Test::test11(){
+
+bool Test::test15(){
+    cout << "Test 15: ";
     return true;
 }
-bool Test::test12(){
+
+bool Test::test16(){
+    cout << "Test 16: ";
     return true;
 }
+
+bool Test::test17(){
+    cout << "Test 17: ";
+    return true;
+}
+
+bool Test::test18(){
+    cout << "Test 18: ";
+    return true;
+}
+
+bool Test::test19(){
+    cout << "Test 19: ";
+    return true;
+}
+
+bool Test::test20(){
+    cout << "Test 20: ";
+    return true;
+}
+
+bool Test::test21(){
+    cout << "Test 21: ";
+    return true;
+}
+
+bool Test::test22(){
+    cout << "Test 22: ";
+    return true;
+}
+
+bool Test::test23(){
+    cout << "Test 23: ";
+    return true;
+}
+
+bool Test::test24(){
+    cout << "Test 24: ";
+    return true;
+}
+
+bool Test::test25(){
+    cout << "Test 25: ";
+    return true;
+}
+
+bool Test::test26(){
+    cout << "Test 26: ";
+    return true;
+}
+
+bool Test::test27(){
+    cout << "Test 27: ";
+    return true;
+}
+
+bool Test::test28(){
+    cout << "Test 28: ";
+    return true;
+}
+
 
 void Test::printResult(bool success){
     if(success){
--- Test.h
+++ Test.h
@@ -13,65 +13,38 @@
 class Test {
 private:
 
-    /**
-     * @brief Creates an empty list and verifies isEmpty() returns true
-     **/
-    bool test01();
+    
 
-    /**
-     * @brief Creates an empty list adds 1 value, verifies isEmpty() returns false
-     **/
-    bool test02();
+    bool test01(); // isEmpty() on empty list returns true
+    bool test02(); // isEmpty() returns false after addFront()
+    bool test03(); // isEmpty() returns false after addBack()
+    bool test04(); // size() returns 0 when empty list
+    bool test05(); // size() returns correctly after 1 addFront()
+    bool test06(); // size() returns correctly after 1 addBack()
+    bool test07(); // size() returns correctly after multiple addFront()
+    bool test08(); // size() returns correctly after multiple addBack()
+    bool test09(); // addFront() properly adds to empty list
+    bool test10(); // addBack() properly adds to empty list
+    bool test11(); // nth addFront() properly adds to front of list
+    bool test12(); // nth addBack() properly adds to the back of the list
+    bool test13(); // removeFront() returns false on empty list
+    bool test14(); // removeBack() returns false on empty list
+    bool test15(); // removeFront() returns true removing from 1 element list
+    bool test16(); // removeBack() returns true removing from 1 element list
+    bool test17(); // removeFront() returns true removing from multiple element list
+    bool test18(); // removeBack() returns true removing from multiple element list
+    bool test19(); // isEmpty() returns true after removeFront() from 1 element list
+    bool test20(); // isEmpty() returns true after removeBack() from 1 element list
+    bool test21(); // size() returns correctly after removeFront() on empty list
+    bool test22(); // size() returns correctly after removeBack() on empty list
+    bool test23(); // size() returns correctly after removeFront() on multiple element list
+    bool test24(); // size() returns correctly after removeBack() on multiple element list
+    bool test25(); // search() returns true when element is in list
+    bool test26(); // search() returns true when element is at beginning of list
+    bool test27(); // search() returns true when element is at end of list
+    bool test28(); // search() returns false when element is not list
 
-    /**
-     * @brief size() returns 0 when empty list
-     **/
-    bool test03();
-
-    /**
-     * @brief size() returns 1 when list of 1 int
-     **/
-    bool test04();
-
-    /**
-     * @brief use addFront once to empty list and check that it is at front of array
-     **/
-    bool test05();
     
-    /**
-     * @brief use addFront twice to empty list and check that the second is at front of array
-     **/
-    bool test06();
-    
-    /**
-     * @brief use addBack once to empty list and check that it is at back of array
-     **/
-    bool test07();
-    
-    /**
-     * @brief use addBack twice to empty list and check that the second is at back of array
-     **/
-    bool test08();
-    
-    /**
-     * @brief use addFront once to empty list and check that it is at front of array
-     **/
-    bool test09();
-    
-    /**
-     * @brief
-     **/
-    bool test10();
-    
-    /**
-     * @brief
-     **/
-    bool test11();
-    
-    /**
-     * @brief
-     **/
-    bool test12();
 
     /**
     * @brief outputs passed or failed to console
