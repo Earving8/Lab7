47b736bed867342b97437a268028ac4b30a0f7dc
Earving Morales <earving_08@hotmail.hotmail.com>
2019-04-25 22:05:14 -0500

    added tests for size() addBack() and addFront()

--- Test.cpp
+++ Test.cpp
@@ -18,26 +18,87 @@ Test::Test(){
 void Test::runTests(){
     printResult(test01());
     printResult(test02());
+    printResult(test03());
+    printResult(test04());
+    printResult(test05());
+    printResult(test06());
+    printResult(test07());
+    printResult(test08());
 }
 
 
 bool Test::test01(){
     LinkedListOfInts list;
-    cout << "Test #1: isEmpty() returns true with an empty list     ";
+    cout << "Test 1: isEmpty() returns true with an empty list";
     return(list.isEmpty());
 }
 
 bool Test::test02(){
     LinkedListOfInts list;
     list.addFront(1);
-    cout << "Test #2: isEmpty() returns false with a nonempty list     ";
+    cout << "Test 2: isEmpty() returns false with a nonempty list";
     return(!list.isEmpty());
+}
+
+bool Test::test03(){
+    cout << "Test 3: size() returns 0 when empty list";
+    LinkedListOfInts list;
+    return(list.size() == 0);
+}
+
+bool Test::test04(){
+    cout << "Test 4: size() returns 1 when list of 1 int";
+    LinkedListOfInts list;
+    list.addFront(2);
+    return(list.size() == 1);
+}
+
+bool Test::test05(){
+    cout << "Test 5: use addFront() once to empty list and check that it is at front of list";
+    LinkedListOfInts list;
+    list.addFront(3);
+    return(list.toVector().front() == 3);
+}
+
+bool Test::test06(){
+    cout << "Test 6: use addFront() twice to empty list and check that the second is at front of list";
+    LinkedListOfInts list;
+    list.addFront(3);
+    list.addFront(4);
+    return(list.toVector().front() == 4);
+}
+
+bool Test::test07(){
+    cout << "Test 7: use addBack() once to empty list and check that it is at back of array";
+    LinkedListOfInts list;
+    list.addBack(5);
+    return(list.toVector().back() == 5);
+}
+
+bool Test::test08(){
+    cout << "Test 8: use addBack() twice to empty list and check that the second is at back of array";
+    LinkedListOfInts list;
+    list.addBack(5);
+    list.addBack(6);
+    return(list.toVector().back() == 6);
+}
+bool Test::test09(){
+    return true;
+}
+bool Test::test10(){
+    return true;
+}
+bool Test::test11(){
+    return true;
+}
+bool Test::test12(){
+    return true;
 }
 
 void Test::printResult(bool success){
     if(success){
-        cout << "Passed\n";
+        cout << " --Passed\n";
     }else{
-        cout << "Failed\n";
+        cout << " --Failed\n";
     }
 }
--- Test.h
+++ Test.h
@@ -1,5 +1,5 @@
 /**
-*	@file Test.hpp
+*	@file Test.h
 *	@author Earving
 *	@date 4/24/19
 *	@brief A header for Test Class
@@ -13,20 +13,70 @@
 class Test {
 private:
 
-        /**
-         * @brief Creates an empty list and verifies isEmpty() returns true
-         **/
-        bool test01();
+    /**
+     * @brief Creates an empty list and verifies isEmpty() returns true
+     **/
+    bool test01();
 
-        /**
-         * @brief Creates an empty list adds 1 value, verifies isEmpty() returns false
-         **/
-        bool test02();
+    /**
+     * @brief Creates an empty list adds 1 value, verifies isEmpty() returns false
+     **/
+    bool test02();
 
-        /**
-        * @brief outputs passed or failed to console
-        **/
-        void printResult(bool success);
+    /**
+     * @brief size() returns 0 when empty list
+     **/
+    bool test03();
+
+    /**
+     * @brief size() returns 1 when list of 1 int
+     **/
+    bool test04();
+
+    /**
+     * @brief use addFront once to empty list and check that it is at front of array
+     **/
+    bool test05();
+    
+    /**
+     * @brief use addFront twice to empty list and check that the second is at front of array
+     **/
+    bool test06();
+    
+    /**
+     * @brief use addBack once to empty list and check that it is at back of array
+     **/
+    bool test07();
+    
+    /**
+     * @brief use addBack twice to empty list and check that the second is at back of array
+     **/
+    bool test08();
+    
+    /**
+     * @brief use addFront once to empty list and check that it is at front of array
+     **/
+    bool test09();
+    
+    /**
+     * @brief
+     **/
+    bool test10();
+    
+    /**
+     * @brief
+     **/
+    bool test11();
+    
+    /**
+     * @brief
+     **/
+    bool test12();
+
+    /**
+    * @brief outputs passed or failed to console
+    **/
+    void printResult(bool success);
 
 
 public:
